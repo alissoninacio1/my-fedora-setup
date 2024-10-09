f6df30aaa56171712df01b3407358cea1eac738e
Álisson Inácio <69402669+alissoninacio1@users.noreply.github.com>
2024-10-09 09:21:25 -0700

    Add files via upload

---
+++ atualiza_scripts.py
@@ -0,0 +1,14 @@
+import os
+
+# Caminho para a pasta onde estão os scripts
+scripts_folder = os.path.expanduser('~/MyFedoraSetup')
+
+# Atualizar todos os scripts na pasta MyFedoraSetup
+for script_name in os.listdir(scripts_folder):
+    script_path = os.path.join(scripts_folder, script_name)
+    if os.path.isfile(script_path) and script_name.endswith('.sh'):
+        print(f'Atualizando o script: {script_name}')
+        # Aqui você pode adicionar os comandos para atualizar cada script
+        # Exemplo: os.system(f'cp /path/to/new/script.sh {script_path}')
+
+print("Atualização concluída.")
